096a96e105771fb80288459549f943aa7413cf29
Michael Tweten <Michael.Tweten@cerner.com>
2018-02-10 23:23:38 -0600

    Trying out stuff

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ if [ -z "$1" ]; then
   echo "Usage: $0 <version>"
   exit 1
 fi
-docker run --rm --privileged -v ~/.docker:/root/.docker -v $(pwd):/data homeassistant/amd64-builder --amd64 -t . --homeassistant $1 -d jvolkman
+docker run --rm --privileged -v ~/.docker:/root/.docker -v $(pwd):/data homeassistant/amd64-builder -t . --homeassistant-machine "$1"=intel-nuc -d mtweten
